d364fec8ff3f2488993d9635084069aa92a50144
vinla <vinla@hotmail.co.uk>
2018-11-06 22:04:40 +0000

    Marten

--- Cogslite/CogsLite.MartenStore/ImageStore.cs
+++ Cogslite/CogsLite.MartenStore/ImageStore.cs
@@ -1,18 +1,28 @@
+using System.IO;
 using System.Threading.Tasks;
 using CogsLite.Core;
 
 namespace CogsLite.MartenStore
 {
     public class ImageStore : IImageStore
-    {
+    {        
         public Task Add(ImageData imageData)
         {
-            throw new System.NotImplementedException();
+            File.WriteAllBytes($"image_data\\{imageData.Id}.dat", imageData.Data);
+            return Task.CompletedTask;
         }
 
         public Task<ImageData> Get(string id)
-        {
-            throw new System.NotImplementedException();
+        {            
+            var result = new ImageData
+            {
+                Id = id,
+                Data = File.ReadAllBytes($"image_data\\{id}.dat"),
+                OriginalFileName = string.Empty,
+                Version = 1
+            };
+
+            return Task.FromResult(result);
         }
     }
 }
