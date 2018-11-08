815ed74f01a02aad0a1d4e7db51a2c1dd6ea3d51
vinla <vinla@hotmail.co.uk>
2018-11-08 22:27:28 +0000

    Deck controller methods

--- Cogslite/CogsLite.Api/Controllers/DeckController.cs
+++ Cogslite/CogsLite.Api/Controllers/DeckController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CogsLite.Api.Payloads;
@@ -25,6 +26,51 @@ namespace CogsLite.Api.Controllers
 		{
 			var decks = await _deckStore.ByGameAndOwner(gameId, _userContext.SignedInUser.Id);
 			return new JsonResult(decks.Select(DeckData.FromDeck));
+		}
+
+        [HttpGet("/api/deck/{deckId}")]
+        public async Task<IActionResult> OnGet(Guid deckId)
+		{			
+			var deck = await _deckStore.Get(deckId);
+			return new JsonResult(DeckData.FromDeck(deck));
+		}
+
+        [HttpPost("/api/deck")]
+		public async Task<IActionResult> OnPostDeck([FromBody]DeckData deck)
+		{
+			var theDeck = deck.ToDeck();
+			theDeck.Owner = _userContext.SignedInUser;
+			theDeck.Version++;
+			await _deckStore.Save(theDeck);
+			return new JsonResult(DeckData.FromDeck(theDeck));
+		}		
+
+        [HttpGet("/api/deck/{deckId}/sheet")]
+		public async Task<IActionResult> OnGetSheet(Guid deckId)
+		{	
+			var deck = await _deckStore.Get(deckId);	
+			var sheet = await _imageStore.Get("Deck", deckId);
+
+			if(sheet == null)
+			{
+				var cardCount = deck.Items.Sum(i => i.Amount);
+				var cardSheetData = ImageSlicer.Composite(DataImages(deck), cardCount);				
+				await _imageStore.Add("Deck", deckId, "png", cardSheetData);
+				return new FileContentResult(cardSheetData, "image/png");
+			}
+			
+			return new FileContentResult(sheet, "image/png");
+		}
+
+		private IEnumerable<byte[]> DataImages(Deck deck)
+		{
+			foreach (var item in deck.Items)
+			{
+				for (int i = 0; i < item.Amount; i++)
+				{
+					yield return _imageStore.Get("Card",  item.CardId).GetAwaiter().GetResult();
+				}
+			}
 		}
     }
 }
